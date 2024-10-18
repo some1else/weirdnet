1304f8f3f25a719a1ab84a8d35cdd62505b4de5b
Srđan Prodanović <srdjan.prodanovic@gmail.com>
2024-10-18 03:35:59 +0200

    Modify grafpac to create the Weirdnet demo

--- src/App.js
+++ src/App.js
@@ -1,123 +1,211 @@
 import React, { PureComponent } from "react"
-import { ForceGraph, ForceGraphNode, ForceGraphLink } from "react-vis-force"
-// import { primes } from "./util"
 import Color from "color"
+import { ForceGraph, ForceGraphNode, ForceGraphLink } from "react-vis-force"
+
+// import { primes } from "./util"
 
 import seedData from "./seedData.json"
 
-import { logGraph, grow, findLooseEdges } from "./Graph"
+import { grow, resetIds } from "./Graph"
 
 import "./App.css"
 
-const VERTEX_COUNT = Math.floor(1024 / 3)
+import { words as lorem_ipsum_words } from "./lorem_ipsum"
+
+const excluded = ["gps", "irs"]
+
+const VERTEX_COUNT = 342
+
+const STAGES = [
+  {
+    stage: 0,
+    verticeCount: 86,
+    animateDelayMs: 20,
+    delay: 10000,
+  },
+  {
+    stage: 1,
+    verticeCount: 114,
+    animateDelayMs: 50,
+    delay: 12000,
+  },
+  {
+    stage: 2,
+    verticeCount: 171,
+    animateDelayMs: 40,
+    delay: 28000,
+  },
+  {
+    stage: 3,
+    verticeCount: 228,
+    animateDelayMs: 30,
+    delay: 38000,
+  },
+  {
+    stage: 4,
+    verticeCount: 342,
+    animateDelayMs: 20,
+    delay: 56000,
+  },
+  // {
+  //   stage: 5,
+  //   verticeCount: 512,
+  //   animateDelayMs: 0,
+  //   delay: 64000,
+  // },
+]
 
 function hexColorFor(id) {
   const color = Color(`hsl(${id % 360}, 100%, 50%)`)
   return color.hex()
 }
 
-class App extends PureComponent {
-  state = { ...seedData, animate: false }
+let wordmap = new Map()
+let mapped = []
 
-  growGraph = () => {
+const weird_words = process.env.REACT_APP_WEIRD_WORDS
+  ? process.env.REACT_APP_WEIRD_WORDS.split(",")
+  : lorem_ipsum_words
+
+function getWord(id) {
+  let word = wordmap.get(id)
+  if (!word) {
+    word = weird_words[Math.floor(Math.random() * weird_words.length)]
+    let retries = 0
+    while ((mapped.includes(word) || excluded.includes(word)) && retries < 3) {
+      word = weird_words[Math.floor(Math.random() * weird_words.length)]
+      retries++
+    }
+    word = word.replace(/_/g, " ")
+    mapped.push(word)
+    word = word.substr(0, 1).toUpperCase() + word.substr(1, word.length)
+    wordmap.set(id, word)
+  }
+
+  return word
+}
+
+function getSeedData() {
+  return JSON.parse(JSON.stringify(seedData))
+}
+
+class App extends PureComponent {
+  state = {
+    ...getSeedData(),
+    stage: 0,
+    totalStages: 0,
+    showLabel: false,
+  }
+
+  animateGraph = () => {
     const { edges, vertices } = this.state
 
-    if (vertices.length === Math.floor(VERTEX_COUNT / 4)) {
-      this.setState({ ...this.state, animate: true })
-      clearInterval(this.interval)
+    if (vertices.length >= STAGES[this.state.stage].verticeCount) {
+      clearInterval(this.animateInterval)
+
+      const nextStage =
+        this.state.stage + 1 === STAGES.length ? 0 : this.state.stage + 1
 
       setTimeout(() => {
-        this.interval = setInterval(this.growGraph, 20)
-      }, 6000)
+        if (nextStage === 0) {
+          wordmap = new Map()
+          mapped = []
+          resetIds()
+
+          this.setState({
+            ...getSeedData(),
+            stage: nextStage,
+            showLabel: false,
+          })
+        } else {
+          this.setState({
+            ...this.state,
+            stage: nextStage,
+            showLabel: false,
+          })
+        }
+
+        this.animateInterval = setInterval(
+          this.animateGraph,
+          STAGES[this.state.stage].animateDelayMs
+        )
+      }, STAGES[this.state.stage].delay)
+
+      this.setState({
+        ...this.state,
+        showLabel: true,
+        totalStages: this.state.totalStages + 1,
+      })
+
+      return
     }
 
-    if (vertices.length === Math.floor(VERTEX_COUNT / 3)) {
-      clearInterval(this.interval)
+    let { graph } = grow({ edges, vertices }, { test: true })
 
-      setTimeout(() => {
-        this.interval = setInterval(this.growGraph, 20)
-      }, 10000)
-    }
-
-    if (vertices.length === Math.floor(VERTEX_COUNT / 2)) {
-      clearInterval(this.interval)
-
-      setTimeout(() => {
-        this.interval = setInterval(this.growGraph, 10)
-      }, 15000)
-    }
-
-    if (vertices.length === Math.floor(VERTEX_COUNT / 1.5)) {
-      clearInterval(this.interval)
-
-      setTimeout(() => {
-        this.interval = setInterval(this.growGraph, 8)
-      }, 38000)
-    }
-
-    if (vertices.length >= VERTEX_COUNT) {
-      clearTimeout(this.interval)
-
-      logGraph({ edges, vertices })
-    }
-
-    // debugger
-
-    let newState = grow({ edges, vertices }, { test: true })
-
-    // console.log("loose ends:", findLooseEdges(newState).length)
-
-    this.setState(newState.graph)
+    this.setState({
+      ...this.state,
+      edges: graph.edges,
+      vertices: graph.vertices,
+    })
   }
 
   componentDidMount() {
-    const { vertices, edges } = this.state
-    this.interval = setInterval(this.growGraph, 20)
+    this.animateInterval = setInterval(
+      this.animateGraph,
+      STAGES[0].animateDelayMs
+    )
   }
 
   render() {
-    const { vertices, edges, animate } = this.state
+    const { vertices, edges } = this.state
 
     return (
       <div className="App">
         <div className="description">
-          Mesh building logic:
-          <br />
-          - It costs two connections to add new nodes.
-          <br />- New nodes are added on the outer perimeter of the mesh.
+          {this.state.totalStages < 5 && (
+            <>
+              Shuffled concentric mind-map on a hexagonal lattice
+              <br />
+            </>
+          )}
+          {this.state.totalStages < 3 && (
+            <>
+              =======================================
+              <br />
+            </>
+          )}
+          {this.state.vertices.length} WordNet concepts,{" "}
+          {this.state.edges.length} associations
+          {this.state.totalStages < 3 && (
+            <>
+              <br />
+              - It costs two vertices to add a new node.
+              <br />- New nodes are added on the perimeter.
+            </>
+          )}
         </div>
-        {!animate && (
-          <h1
-            style={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              textAlign: "center",
-              fontWeight: "normal",
-              color: "#444",
-              lineHeight: "180%",
-            }}
-          >
-            <small>
-              Building graph...
-              <br />
-              {vertices.length} vertices, {edges.length} edges
-              <br />
-              {Math.ceil((vertices.length / VERTEX_COUNT) * 100)}% complete
-            </small>
+
+        {this.state.totalStages === 0 && (
+          <h1 className="loading-text">
+            Building WeirdNet
+            {/* <br />
+            {vertices.length} concepts, {edges.length} edges */}
+            <br />
+            {Math.ceil((vertices.length / VERTEX_COUNT) * 100)}% complete
           </h1>
         )}
-        {animate && (
+
+        {this.state.totalStages > 0 && (
           <ForceGraph
+            labelAttr="label"
+            className={`stage-${this.state.stage} showLabel-${this.state.showLabel}`}
             simulationOptions={{
-              animate,
-              labelAttr: "id",
+              animate: true,
               // alphaDecay: 0.0001,
-              alphaDecay: 0.000001,
-              velocityDecay: 0.35,
+              alphaDecay: 0.0000001,
+              velocityDecay: 0.375,
               strength: {
-                charge: (1 / this.state.vertices.length) * -10000,
+                charge: (1 / this.state.vertices.length) * -48000,
               },
               width: window.innerWidth,
               height: window.innerHeight,
@@ -125,14 +213,17 @@ class App extends PureComponent {
           >
             {vertices.map(({ id }) => (
               <ForceGraphNode
-                node={{ id }}
+                node={{
+                  id,
+                  label: `${id}. ${getWord(id)}`,
+                }}
                 // fill={primes.indexOf(id) === -1 ? "lightgray" : "cyan"}
                 fill={hexColorFor(id)}
                 key={`vertex-${id}`}
-                showLabel
+                showLabel={this.state.showLabel}
+                r={this.state.showLabel ? 3.75 : 3.75}
                 // cx={Math.random() * window.innerWidth}
                 // cy={Math.random() * window.innerHeight}
-                r={3.5}
               />
             ))}
             {edges.map(({ source, target }) => (
